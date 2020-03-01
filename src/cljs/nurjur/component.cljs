02ff8dc0cd2e18e66e68a6cec932b05f2d314978
Dave Simmons <shortlyportly@googlemail.com>
2020-03-01 08:45:50 +0000

    Submit now validates before posting - does not actually submit yet

--- src/cljs/nurjur/component.cljs
+++ src/cljs/nurjur/component.cljs
@@ -12,17 +12,28 @@
   (or (seq (field-name @n/db))
       (not (get-in @n/db [:optional field-name]))))
 
-(defn validate [field-name validations]
+(defn validate-field! [field-name validations]
   "Determine if the given field is valid."
   (if (validate? field-name)
     (if-let [error (first (st/validate @n/db {field-name validations}))]
       (swap! n/db assoc :errors (merge (:errors @n/db) error))
-      (swap! n/db assoc :errors (merge (:errors @n/db) {field-name nil})))))
 
-(defn validate-fields [fields]
-  "Validate all the fields passed in the collection"
-  (for [field fields]
-    (validate (:name field) (:validations field))))
+      (swap! n/db assoc :errors (dissoc (:errors @n/db) field-name)))))
+
+(defn validate-fields! [fields]
+  "Validate all the fields passed in the collection.
+
+  Note the use of doseq which is required as we are not using the returning values simply
+  forcing the update of the db."
+  (doseq [field fields] (validate-field! (:name field) (:validations field))))
+
+(defn validate-form! [fields form-validations]
+  "Pefrom cross field validations")
+
+(defn form-valid? [fields]
+  (validate-fields! fields)
+  ;(validate-form! (:fields args) (:form-validations args))
+  (empty? (:errors @n/db)))
 
 (defn section [content]
   [:section.section>div.container>div.content
@@ -30,57 +41,75 @@
 
 (defn text-input [args]
   (swap! n/db assoc :optional (merge (:optional @n/db) {(:name args) (:optional args)}))
-  (js/console.log (:default args))
   (swap! n/db assoc (:name args) (:default args))
-  (js/console.log @n/db)
   (fn []
     [:div.field
      [:label.label (:label args)]
      [:div.control
       [:input.input
        {:type :text
-        :name (str (namespace (:name args)) "-" (name(:name args)))
+        :name (str (namespace (:name args)) "-" (name (:name args)))
         :defaultValue ((:name args) @n/db)
         :on-blur #((swap! n/db assoc (:name args) (-> % .-target .-value))
-                   (validate (:name args) (:validations args)))}]
+                   (validate-field! (:name args) (:validations args)))}]
 
-     (if-let [error-text (get-in @n/db [:errors (:name args)])]
-       [:p.help.is-danger error-text])]]))
+      (if-let [error-text (get-in @n/db [:errors (:name args)])]
+        [:p.help.is-danger error-text])]]))
 
-(defn form [ args]
+(defn form [args]
   (fn []
     [:form
      (for [field (:fields args)]
        [text-input field])
 
-     [:button.button.is-primary {:on-click
-                                 #(do
-                                   (.preventDefault %)
-                                   (js/console.log "click"))}
-      "wibble2"]
-
-     ]))
+     [:button.button.is-primary
+      {:on-click
+       #(do
+          (.preventDefault %)
+          (if (form-valid? (:fields args))
+            (js/console.log "form valid")
+            (js/console.log "form invalid")))}
+      "wibble2"]]))
 
 ; Test data
+
 
 (defn validate-length [length]
   {:message (str "Length must be greater than " length)
    :validate #(> (count %) length)})
 
 (def test-fields
-    {:fields
-    [{:type :text
-      :label "First Name3"
-      :name :user/first-name
-      :default "David"
-      :validations [st/required st/string (validate-length 2)]}
-     {:type :text
-      :label "Middle Name"
-      :optional true
-      :name :user/middle-name
-      :validations [st/required st/string (validate-length 2)]}
-     {:type :text
-      :label "Last Name"
-      :name :user/last-name
-      :default "Simmons"
-      :validations [st/required st/string (validate-length 2)]}]})
+  {:fields
+   [{:type :text
+     :label "First Name3"
+     :name :user/first-name
+     :default "David"
+     :validations [st/required st/string (validate-length 2)]}
+    {:type :text
+     :label "Middle Name"
+     :optional true
+     :name :user/middle-name
+     :validations [st/required st/string (validate-length 2)]}
+    {:type :text
+     :label "Last Name"
+     :name :user/last-name
+     :default "Simmons"
+     :validations [st/required st/string (validate-length 2)]}]})
+
+(def args-2
+  {:form-name :user/form
+   :fields
+   [{:type :text
+     :label "First Name3"
+     :name :user/first-name
+     :default "David"
+     :validations [st/required st/string (validate-length 2)]}
+    {:type :text
+     :label "Middle Name"
+     :optional true
+     :name :user/middle-name
+     :validations [st/required st/string (validate-length 2)]}
+    {:type :text
+     :label "Last Name"
+     :name :user/last-name
+     :validations [st/required st/string (validate-length 2)]}]})
